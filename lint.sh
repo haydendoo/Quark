dbe8ed074e916aae8769ec58260e5cfeae39009b
haydendoo <haydenhow@gmail.com>
2025-05-15 15:10:24 +0800

    fix: Modify code to include

--- lint.sh
+++ lint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find src includes \( -name '*.cpp' -o -name '*.hpp' \) | xargs -n 1 clang-tidy -header-filter=.* -p build
+find src include \( -name '*.cpp' -o -name '*.hpp' \) | xargs -n 1 clang-tidy -header-filter=.* -p build
